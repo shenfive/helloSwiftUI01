e8326d30e979f4b0de2c42905f71a1a4eb0e7603
Danny Shen <shenfive@gmail.com>
2019-12-28 11:23:17 +0800

    自訂 View

--- helloSwiftUI01/ContentView.swift
+++ helloSwiftUI01/ContentView.swift
@@ -9,18 +9,29 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var inputText:String
+
+    @State var red = 0
+    @State var green = 1
+    @State var blue = 2
     var body: some View {
         VStack{
-            Text(inputText)
-            TextField("", text: $inputText)
-                .frame(width: 100.0)
+            MyText(text: "RED:\(red)")
+            MyText(text:"RED:\(green)")
+            MyText(text:"RED:\(blue)")
         }
+    }
+}
+
+struct MyText:View {
+    var text:String
+    var body:some View{
+        Text(text)
+        .font(.system(size: 25))
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(inputText: "tesSTER")
+        ContentView()
     }
 }
